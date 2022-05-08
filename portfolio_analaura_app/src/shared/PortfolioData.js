3b7ab907083f0a4aaf19d9728bea7bfb58324f1d
Thomas Aleknovic <thomasaleknovic@Thomass-MacBook-Pro.local>
2022-05-08 09:54:13 -0300

    Version 1.5.0

--- portfolio_analaura_app/src/shared/PortfolioData.js
+++ portfolio_analaura_app/src/shared/PortfolioData.js
@@ -6,14 +6,14 @@ export const PORTFOLIODATA = [
         id: 1,
         name: 'Three Dogs & Cats',
         description: 'Ação para o programa ”Posso entrar?” da RBS.',
-        cover: '/assets/Three Dogs/portfolio_threeDogs_1.png',
-        banner: ['/portfolio_ana_laura/portfolio_analaura_app/assets/Three Dogs/portfolio_threeDogs_main_banner.png'],
+        cover: './assets/Three Dogs/portfolio_threeDogs_1.png',
+        banner: ['./assets/Three Dogs/portfolio_threeDogs_main_banner.png'],
         projects: [
             {
             id: 0,
             title: 'Posso entrar?',
-            images:['/assets/Three Dogs/portfolio_threeDogs_2.png', '/assets/Three Dogs/portfolio_threeDogs_3.png','/assets/Three Dogs/portfolio_threeDogs_4.png', '/assets/Three Dogs/portfolio_threeDogs_5.png', '/assets/Three Dogs/portfolio_threeDogs_6.png'],
-            imagesTwo: ['/assets/Three Dogs/portfolio_threeDogs_8.png'],
+            images:['./assets/Three Dogs/portfolio_threeDogs_2.png', '/assets/Three Dogs/portfolio_threeDogs_3.png','/assets/Three Dogs/portfolio_threeDogs_4.png', '/assets/Three Dogs/portfolio_threeDogs_5.png', '/assets/Three Dogs/portfolio_threeDogs_6.png'],
+            imagesTwo: ['./assets/Three Dogs/portfolio_threeDogs_8.png'],
             description:'Idealização do quadro sobre adoção da Chica + ideias de presente + redação do certificado pet three + momento merchan com os produtos.'
             
             }
