cf2231079cfce4f9f83408f05595deab3d968022
Thomas Aleknovic <thomasaleknovic@Thomass-MacBook-Pro.local>
2022-05-09 13:06:02 -0300

    Version 2.6.0

--- portfolio_analaura_app/src/Components/ContactUs.jsx
+++ portfolio_analaura_app/src/Components/ContactUs.jsx
@@ -18,13 +18,13 @@ export const ContactUs = () => {
   return(
   
       <div className='flex justify-center'>
-        <form ref={form} onSubmit={sendEmail} className="bg-secondary-color rounded-lg w-6/5 md:w-2/4  flex flex-col items-center text-main-pink text-xl p-10" >
+        <form ref={form} onSubmit={sendEmail} className="bg-secondary-color rounded-lg w-5/6 md:w-2/4 flex flex-col items-center text-main-pink text-base md:text-xl p-10" >
           <label>Nome</label>
-          <input type="text" name="name" className='mt-2 mb-10 rounded-full p-2 text-center'/>
+          <input type="text" name="name" className='mt-2 mb-10 rounded-full p-2 text-center w-[250px] md:w-3/4'/>
           <label>Email</label>
-          <input type="email" name="email" className='mt-2 mb-10 rounded-full p-2 text-center'/>
+          <input type="email" name="email" className='mt-2 mb-10 rounded-full p-2 text-center w-[250px] md:w-3/4'/>
           <label>Mensagem</label>
-          <textarea name="message" className='mt-2 mb-2 rounded-lg p-2 text-center h-[150px]' />
+          <textarea name="message" className='mt-2 mb-2 rounded-lg p-2 text-center h-[150px] w-[250px] md:w-3/4' />
           <input type="submit" value="Enviar" className='mt-8 px-14 py-1 rounded-full bg-main-pink text-secondary-color cursor-pointer'/>
           </form>
       </div>
